b7c645bdaacdba10aed0ac67220b6fe08a6a1080
Gullit Miranda <gullitmiranda@requestdev.com.br>
2014-08-01 16:30:56 -0300

    add RAKE_ENV

--- Guardfile
+++ Guardfile
@@ -1,3 +1,5 @@
+ENV["RACK_ENV"] = "test"
+
 guard :rspec, cmd: 'rspec' do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
--- lib/config/meli.rb
+++ lib/config/meli.rb
@@ -4,7 +4,11 @@ Meli.configure do |config|
   config.client_secret  = ENV['MERCADOLIBRE_APP_SECRET'   ]
   config.callback_url   = ENV['MERCADOLIBRE_CALLBACK_URL' ]
 
-
+  # Envs for development
+  if ENV["RACK_ENV"] == "test" and ENV["RACK_ENV"] != "production"
+    config.client_id      = "1551683306174268"
+    config.client_secret  = "jVig8f30muP7JXgk8AsP6R4icK9wDj2C"
+  end
 
   # Site Country
   # For other country check https://api.mercadolibre.com/sites/
