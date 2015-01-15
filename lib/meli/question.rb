46a863ddcccd58f97fbffa8563c54df12e9c7c40
Marcus Gadbem <mcsgad@gmail.com>
2015-01-15 01:56:45 -0200

    Fixes and new endpoitnts * Renamed 'search_by_item_id' to 'find_by_item_id' * Added endpoints to add/del/list blacklisted users

--- lib/meli/question.rb
+++ lib/meli/question.rb
@@ -12,10 +12,16 @@ module Meli
       instantiate_record record, prefix_options
     end
 
-    def self.search_by_item_id(item_id)
+    def self.find(question_id)
+      path    = "/questions/#{item_id}"
+      data    = format.decode(connection.get(path, headers).body)
+      instantiate_record data
+    end
+
+    def self.find_by_item_id(item_id)
       path    = "/questions/search?item_id=#{item_id}"
       data    = format.decode(connection.get(path, headers).body)
-      instantiate_collection data
+      instantiate_record data
     end
 
     def self.ask_question(item_id, question="")
@@ -28,7 +34,7 @@ module Meli
       instantiate_record data
     end
 
-    def self.answer_question(question_id= "", text="")
+    def self.answer_question(question_id="", text="")
       path    = "/answers"
       headers = {
         "question_id" => question_id,
@@ -36,6 +42,36 @@ module Meli
       }
       data    = format.decode(connection.post(path, headers).body)
       instantiate_record data
+    end
+
+    def self.list_blacklist(user_id)
+      path    = "/users/#{user_id}/questions_blacklist"
+      data    = format.decode(connection.get(path, headers).body)
+      instantiate_collection data
+    end
+
+     def self.add_user_to_blacklist(user_id)
+      path    = "/users/#{user_id}/questions_blacklist"
+      headers = {
+        "user_id"   => user_id
+      }
+      data    = format.decode(connection.post(path, headers).body)
+      instantiate_collection data
+    end
+
+    def self.remove_user_from_blacklist(user_id)
+      path    = "/users/#{user_id}/questions_blacklist"
+      headers = {
+        "user_id"   => user_id
+      }
+      data    = format.decode(connection.delete(path, headers).body)
+      instantiate_collection data
+    end
+
+    def self.all_my_questions(user_id)
+      path    = "/my/received_questions/search"
+      data    = format.decode(connection.get(path, headers).body)
+      instantiate_collection data
     end
 
   end
